2855accd7ec47839348f0dff6b53023866094c71
fedor <fedor993@gmail.com>
2021-09-14 21:00:21 +0300

    Implemented method to get sushi list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Header } from "./components";
 import { Home, Card } from "./pages";
 import { Route } from "react-router-dom";
 import { api } from "./api";
 
 function App() {
-  
+  const [sushiList, setSushiList] = useState([]);
+
   useEffect(() => {
-    console.log("api", api.get("/sushi"));
+    api.get("/sushi").then((result) => setSushiList(result.sushi));
   }, []);
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home sushi={sushiList} />} />
         <Route exact path="/card" component={Card} />
       </div>
     </div>
