d9078d45bf7b86fef7a56024a8d2a7ceda50c9f0
fedor <fedor993@gmail.com>
2021-09-14 14:29:19 +0300

    Refactor

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,20 +1,16 @@
 import React, { useState } from "react";
 import classNames from "classnames";
 
-export const Categories = ({ sushiTypes, selectType }) => {
-  const [isSelected, setSelectedType] = useState(0);
-
-  const handleSelectedType = (index) => {
-    setSelectedType(index);
-  };
+export const Categories = ({ sushiTypes }) => {
+  const [sushiTypeIndex, setSelectedType] = useState(0);
 
   return (
     <div className="categories">
       <ul>
         {sushiTypes?.map((type, index) => (
           <li
-            className={classNames({ active: isSelected === index })}
-            onClick={() => handleSelectedType(index)}
+            className={classNames({ 'active': sushiTypeIndex === index })}
+            onClick={() => setSelectedType(index)}
             key={index + type}
           >
             {type}
