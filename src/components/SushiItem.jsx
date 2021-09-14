d50b8a63309d0d3aebea21b11da00fa16a93193e
fedor <fedor993@gmail.com>
2021-09-14 22:19:48 +0300

    Added condition to iterate sushi sizes

--- src/components/SushiItem.jsx
+++ src/components/SushiItem.jsx
@@ -3,24 +3,19 @@ import React from 'react'
 export default function SushiItem() {
   return (
     <div className="sushi-block">
-    <img
-      className="sushi-block__image"
-      src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-      alt="Pizza"
-    />
-    <h4 className="sushi-block__title">Чизбургер-пицца</h4>
-    <div className="sushi-block__selector">
-      <ul>
-        <li className="active">тонкое</li>
-        <li>традиционное</li>
-      </ul>
-      <ul>
-        <li className="active">26 см.</li>
-        <li>30 см.</li>
-        <li>40 см.</li>
-      </ul>
-    </div>
-    <div className="sushi-block__bottom">
+        <ul>
+          {sizes?.map((size, index) => (
+            <li
+              class={sushiTypeIndex === index ? "active" : ""}
+              onClick={() => setSelectedSize(index)}
+              key={index}
+            >
+              {size}x
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="sushi-block__bottom">
       <div className="sushi-block__price">от 395 ₽</div>
       <div className="button button--outline button--add">
         <svg
