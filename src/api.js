06d3b6186f02ec06cd91e2a3e44f3170b01091c1
fedor <fedor993@gmail.com>
2021-09-14 21:01:06 +0300

    Renamed JSON fields

--- src/api.js
+++ src/api.js
@@ -6,12 +6,12 @@ export const api = {
           setTimeout(() => {
             resolve(
               {
-                pizzas: [
+                sushi: [
                   {
                     id: 0,
                     imageUrl:
-                      "https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg",
-                    name: "Пепперони Фреш с перцем",
+                      "https://sushivesla.by/img/frontend/dishes/nigiri_with_roast_tuna.png",
+                    name: "Nigiri with roast tuna",
                     types: [0, 1],
                     sizes: [26, 30, 40],
                     price: 803,
@@ -21,8 +21,8 @@ export const api = {
                   {
                     id: 1,
                     imageUrl:
-                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
-                    name: "Сырная",
+                      "https://sushivesla.by/img/frontend/dishes/tamago_new.png",
+                    name: "Tamago",
                     types: [0],
                     sizes: [26, 40],
                     price: 245,
@@ -32,19 +32,19 @@ export const api = {
                   {
                     id: 2,
                     imageUrl:
-                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/6652fec1-04df-49d8-8744-232f1032c44b.jpg",
-                    name: "Цыпленок барбекю",
+                      "https://sushivesla.by/img/frontend/dishes/spicy_maguro-min.png",
+                    name: "Spicy Maguro",
                     types: [0],
                     sizes: [26, 40],
                     price: 295,
                     category: 1,
                     rating: 4,
                   },
-                  {
+                  { 
                     id: 3,
                     imageUrl:
-                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/af553bf5-3887-4501-b88e-8f0f55229429.jpg",
-                    name: "Кисло-сладкий цыпленок",
+                      "https://sushivesla.by/img/frontend/dishes/chuca_mix-min.png",
+                    name: "Chuca mix",
                     types: [1],
                     sizes: [26, 30, 40],
                     price: 275,
@@ -54,8 +54,8 @@ export const api = {
                   {
                     id: 4,
                     imageUrl:
-                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
-                    name: "Чизбургер-пицца",
+                      "https://sushivesla.by/img/frontend/dishes/karai_maki_b.png",
+                    name: "Karai maki",
                     types: [0, 1],
                     sizes: [26, 30, 40],
                     price: 415,
@@ -65,8 +65,8 @@ export const api = {
                   {
                     id: 5,
                     imageUrl:
-                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/1e1a6e80-b3ba-4a44-b6b9-beae5b1fbf27.jpg",
-                    name: "Крэйзи пепперони",
+                      "https://sushivesla.by/img/frontend/dishes/syace_delux_maki.png",
+                    name: "Syace delux maki",
                     types: [0],
                     sizes: [30, 40],
                     price: 580,
@@ -76,8 +76,8 @@ export const api = {
                   {
                     id: 6,
                     imageUrl:
-                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/d2e337e9-e07a-4199-9cc1-501cc44cb8f8.jpg",
-                    name: "Пепперони",
+                      "https://sushivesla.by/img/frontend/dishes/Grand%20Unagi%20maki.png",
+                    name: "Grand Unagi maki",
                     types: [0, 1],
                     sizes: [26, 30, 40],
                     price: 675,
@@ -87,8 +87,8 @@ export const api = {
                   {
                     id: 7,
                     imageUrl:
-                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/d48003cd-902c-420d-9f28-92d9dc5f73b4.jpg",
-                    name: "Маргарита",
+                      "https://sushivesla.by/img/frontend/dishes/250x250.png",
+                    name: "Grand Philadelphia maki",
                     types: [0, 1],
                     sizes: [26, 30, 40],
                     price: 450,
@@ -98,8 +98,8 @@ export const api = {
                   {
                     id: 8,
                     imageUrl:
-                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/ec29465e-606b-4a04-a03e-da3940d37e0e.jpg",
-                    name: "Четыре сезона",
+                      "https://sushivesla.by/img/frontend/dishes/sapporo_maki_b.png",
+                    name: "Sapporo maki",
                     types: [0, 1],
                     sizes: [26, 30, 40],
                     price: 395,
@@ -109,8 +109,8 @@ export const api = {
                   {
                     id: 9,
                     imageUrl:
-                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/30367198-f3bd-44ed-9314-6f717960da07.jpg",
-                    name: "Овощи и грибы 🌱",
+                      "https://sushivesla.by/img/frontend/dishes/sapporo_maki_b.png",
+                    name: "Sapporo maki",
                     types: [0, 1],
                     sizes: [26, 30, 40],
                     price: 285,
