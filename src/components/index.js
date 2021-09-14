0c7d9d9440f4034ae83496aa0deac06dcf287781
fedor <fedor993@gmail.com>
2021-09-14 20:59:56 +0300

    Imported  component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import { Button } from "./Button";
 import { Categories } from "./Categories";
 import { Header } from "./Header";
 import { SortMenu } from "./SortMenu";
+import SushiItem from "./SushiItem";
 
-export { Button, Categories, Header, SortMenu };
+export { Button, Categories, Header, SortMenu, SushiItem };
