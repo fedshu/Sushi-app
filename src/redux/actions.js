75ec0ec0b1b6cab05809daa51b19decc2f936b1c
fedor <fedor993@gmail.com>
2021-09-16 16:42:28 +0300

    Implemented actions

---
+++ src/redux/actions.js
@@ -0,0 +1,9 @@
+export const SET_SORT_TYPE = (type) => ({
+  type: "SET_SORT_TYPE",
+  payload: type,
+});
+
+export const SET_CATEGORY_TYPE = (type) => ({
+  type: "SET_CATEGORY_TYPE",
+  payload: type,
+});
