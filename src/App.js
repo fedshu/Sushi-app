dadff0e53937a44648dfb8a353d76525b03f1357
fedor <fedor993@gmail.com>
2021-09-16 20:51:29 +0300

    Added useDispatch. Refactor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { Header } from "./components";
-import { Home, Card } from "./pages";
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
+import { setSushi } from "./redux/actions";
 import { api } from "./api";
+import { Home, Card } from "./pages";
+import { Header } from "./components";
+import { useDispatch } from "react-redux";
 
 function App() {
-  const [sushiList, setSushiList] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    api.get("/sushi").then((result) => setSushiList(result.sushi));
+    api.get("/sushi").then((result) => dispatch(setSushi(result.sushi)));
   }, []);
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" render={() => <Home sushi={sushiList} />} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/card" component={Card} />
       </div>
     </div>
