8d8656d7f56738f4a45a360b1ced4c55d1f71669
fedor <fedor993@gmail.com>
2021-09-17 19:46:40 +0300

    Added a new paths of category types

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const api = {
   get(url) {
     switch (url) {
-      case "/sushi":
+      case "/sushi/all":
         return new Promise((resolve, reject) => {
           setTimeout(() => {
             resolve({
@@ -14,7 +14,7 @@ export const api = {
                   types: [0, 1],
                   sizes: [1, 2, 8],
                   price: 3.99,
-                  category: 'Yaki maki',
+                  category: "Yaki maki",
                   rating: 4,
                 },
                 {
@@ -24,8 +24,8 @@ export const api = {
                   name: "Tamago",
                   types: [0],
                   sizes: [4, 8],
-                  price: 2.30,
-                  category: 'Yaki maki',
+                  price: 2.3,
+                  category: "Yaki maki",
                   rating: 6,
                 },
                 {
@@ -36,7 +36,7 @@ export const api = {
                   types: [0],
                   sizes: [1, 4, 8],
                   price: 14.99,
-                  category: 'Futomaki',
+                  category: "Futomaki",
                   rating: 4,
                 },
                 {
@@ -47,7 +47,7 @@ export const api = {
                   types: [1],
                   sizes: [1, 4, 8],
                   price: 9.99,
-                  category: 'Futomaki',
+                  category: "Futomaki",
                   rating: 2,
                 },
                 {
@@ -58,7 +58,7 @@ export const api = {
                   types: [0, 1],
                   sizes: [4, 8],
                   price: 6,
-                  category: 'Maki',
+                  category: "Maki",
                   rating: 8,
                 },
                 {
@@ -68,8 +68,8 @@ export const api = {
                   name: "Syace delux maki",
                   types: [0],
                   sizes: [1, 4, 8],
-                  price: 8.20,
-                  category: 'Maki',
+                  price: 8.2,
+                  category: "Maki",
                   rating: 2,
                 },
                 {
@@ -80,7 +80,7 @@ export const api = {
                   types: [0, 1],
                   sizes: [1, 4, 8],
                   price: 2,
-                  category: 'Grand maki',
+                  category: "Grand maki",
                   rating: 9,
                 },
                 {
@@ -90,8 +90,8 @@ export const api = {
                   name: "Grand Philadelphia maki",
                   types: [0, 1],
                   sizes: [4, 8, 16],
-                  price: 1.50,
-                  category: 'Grand maki',
+                  price: 1.5,
+                  category: "Grand maki",
                   rating: 10,
                 },
                 {
@@ -102,8 +102,8 @@ export const api = {
 
                   types: [0, 1],
                   sizes: [4, 8, 16],
-                  price: 1.20,
-                  category: 'Nigiri',
+                  price: 1.2,
+                  category: "Nigiri",
                   rating: 10,
                 },
                 {
@@ -114,10 +114,167 @@ export const api = {
 
                   types: [0, 1],
                   sizes: [4, 8],
-                  price: 2.10,
-                  category: 'Nigiri',
+                  price: 2.1,
+                  category: "Nigiri",
                   rating: 7,
                 },
+              ],
+            });
+          }, 1000);
+        });
+      case "/sushi/yaki-maki":
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve({
+              sushi: [
+                {
+                  id: 0,
+                  imageUrl:
+                    "https://sushivesla.by/img/frontend/dishes/nigiri_with_roast_tuna.png",
+                  name: "Nigiri with roast tuna",
+                  types: [0, 1],
+                  sizes: [1, 2, 8],
+                  price: 3.99,
+                  category: "Yaki maki",
+                  rating: 4,
+                },
+                {
+                  id: 1,
+                  imageUrl:
+                    "https://sushivesla.by/img/frontend/dishes/tamago_new.png",
+                  name: "Tamago",
+                  types: [0],
+                  sizes: [4, 8],
+                  price: 2.3,
+                  category: "Yaki maki",
+                  rating: 6,
+                },
+              ],
+            });
+          }, 1000);
+        });
+      case "/sushi/futomaki":
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve({
+              sushi: [
+                {
+                  id: 2,
+                  imageUrl:
+                    "https://sushivesla.by/img/frontend/dishes/Yaki_syake.jpg",
+                  name: "Yaki syake",
+                  types: [0],
+                  sizes: [1, 4, 8],
+                  price: 14.99,
+                  category: "Futomaki",
+                  rating: 4,
+                },
+                {
+                  id: 3,
+                  imageUrl:
+                    "https://sushivesla.by/img/frontend/dishes/Yaki_tamura.jpg",
+                  name: "Yaki tamura",
+                  types: [1],
+                  sizes: [1, 4, 8],
+                  price: 9.99,
+                  category: "Futomaki",
+                  rating: 2,
+                }
+              ],
+            });
+          }, 1000);
+        });
+      case "/sushi/maki":
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve({
+              sushi: [
+                {
+                  id: 4,
+                  imageUrl:
+                    "https://sushivesla.by/img/frontend/dishes/karai_maki_b.png",
+                  name: "Karai maki",
+                  types: [0, 1],
+                  sizes: [4, 8],
+                  price: 6,
+                  category: "Maki",
+                  rating: 8,
+                },
+                {
+                  id: 5,
+                  imageUrl:
+                    "https://sushivesla.by/img/frontend/dishes/syace_delux_maki.png",
+                  name: "Syace delux maki",
+                  types: [0],
+                  sizes: [1, 4, 8],
+                  price: 8.2,
+                  category: "Maki",
+                  rating: 2,
+                }
+              ],
+            });
+          }, 1000);
+        });
+      case "/sushi/grand-maki":
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve({
+              sushi: [
+                {
+                  id: 6,
+                  imageUrl:
+                    "https://sushivesla.by/img/frontend/dishes/Grand%20Unagi%20maki.png",
+                  name: "Grand Unagi maki",
+                  types: [0, 1],
+                  sizes: [1, 4, 8],
+                  price: 2,
+                  category: "Grand maki",
+                  rating: 9,
+                },
+                {
+                  id: 7,
+                  imageUrl:
+                    "https://sushivesla.by/img/frontend/dishes/250x250.png",
+                  name: "Grand Philadelphia maki",
+                  types: [0, 1],
+                  sizes: [4, 8, 16],
+                  price: 1.5,
+                  category: "Grand maki",
+                  rating: 10,
+                }
+              ],
+            });
+          }, 1000);
+        });
+      case "/sushi/nigiri":
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve({
+              sushi: [
+                {
+                  id: 8,
+                  imageUrl:
+                    "https://sushivesla.by/img/frontend/dishes/sapporo_maki_b.png",
+                  name: "Sapporo maki",
+
+                  types: [0, 1],
+                  sizes: [4, 8, 16],
+                  price: 1.2,
+                  category: "Nigiri",
+                  rating: 10,
+                },
+                {
+                  id: 9,
+                  imageUrl:
+                    "https://sushivesla.by/img/frontend/dishes/shimoda_maki_b.png",
+                  name: "Shimoda maki",
+
+                  types: [0, 1],
+                  sizes: [4, 8],
+                  price: 2.1,
+                  category: "Nigiri",
+                  rating: 7,
+                }
               ],
             });
           }, 1000);
