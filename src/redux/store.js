d8ace7e082b1d877f2387e49d27d35313af9db2a
fedor <fedor993@gmail.com>
2021-09-17 14:16:04 +0300

    Added thunk to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
-import rootReducers from './reducers'
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducers from "./reducers";
 
-const store = createStore(rootReducers)
+const store = createStore(rootReducers, compose(applyMiddleware(thunk)));
 
 export default store;
