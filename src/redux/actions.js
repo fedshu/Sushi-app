0918f15501a2c0f7830f5a3f78b00999518e4b81
fedor <fedor993@gmail.com>
2021-09-18 21:09:54 +0300

    Refactor

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,25 +4,28 @@ export const SET_SORT_TYPE = "SET_SORT_TYPE";
 export const SET_CATEGORY_TYPE = "SET_CATEGORY_TYPE";
 export const SET_SUSHI = "SET_SUSHI";
 export const SET_LOADING = "SET_LOADING";
-export const SET_TOTAL_COUNT = "SET_TOTAL_PRICE";
-export const SET_TOTAL_PRICE = "SET_TOTAL_PRICE";
-
-export const setSortType = (type) => ({
-  type: SET_SORT_TYPE,
-  payload: type,
-});
+export const ADD_SUSHI = "ADD_SUSHI";
+export const setLoading = () => ({ type: SET_LOADING });
 
 export const setCategoryType = (type) => ({
   type: SET_CATEGORY_TYPE,
   payload: type,
 });
 
+export const setSortType = (type) => ({
+  type: SET_SORT_TYPE,
+  payload: type,
+});
+
+export const addSushi = (sushi) => ({
+  type: ADD_SUSHI,
+  payload: sushi,
+})
+
 export const setSushi = (sushi) => ({
   type: SET_SUSHI,
   payload: sushi,
 });
-
-export const setLoading = () => ({ type: SET_LOADING });
 
 export const getSushi = (categoryType, sortType) => (dispatch) => {
   api.get(`/sushi/${categoryType}`).then(({ sushi }) => {
