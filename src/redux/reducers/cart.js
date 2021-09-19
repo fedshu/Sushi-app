d49cee6552462fc5c6278f1106a592be01ac3d3b
fedor <fedor993@gmail.com>
2021-09-19 20:00:27 +0300

    Implemented method to get sushi sizes and prices

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -2,6 +2,7 @@ import { ADD_SUSHI } from "../actions";
 
 const initialState = {
   items: {},
+  selectedSushi: [],
   totalPrice: 0,
   totalCount: 0,
 };
@@ -16,17 +17,56 @@ export const cart = (state = initialState, action) => {
           ? [...stateItems, action.payload]
           : [action.payload],
       };
-      const sushiLength = Object.values(updatedSushi).flat();
-      // const sushiPrice = sushiLength.reduce((sum, obj) => obj.price + sum, 0);
-      const sushiPrice = state.totalPrice + action.payload.price;
+
+      const updatedSushiAsArray = Object.values(updatedSushi);
+      const sushiPrice = state.totalPrice + action.payload.price * (action.payload.size + 1);
 
       return {
         ...state,
         items: updatedSushi,
+        selectedSushi: getSushiSizeAndPrice(updatedSushiAsArray),
         totalPrice: sushiPrice,
-        totalCount: sushiLength.length,
+        totalCount: updatedSushiAsArray.flat().length,
       };
     default:
       return state;
   }
+};
+
+const getSushiSizeAndPrice = (sushi) => {
+  let sizesAndPrices = [];
+
+  //Getting sushi sizes
+  sushi.map((items) => {
+    let sizes = getUniqueSushiSizes(items);
+
+    //Getting sushi length and price
+    Object.values(sizes).forEach((sushiList) => {
+      const price = getSushiSizesTotalAmount(sushiList);
+
+      sizesAndPrices.push({
+        ...sushiList[0],
+        sizeCount: sushiList.length,
+        totalPrice: price,
+      });
+    });
+  });
+
+  return sizesAndPrices;
+};
+
+const getUniqueSushiSizes = (sushi) => {
+  let sizes = {};
+  sushi.map((item) => {
+    sizes[item.size] = sizes[item.size] ? [...sizes[item.size], item] : [item];
+  });
+  return sizes;
+};
+
+const getSushiSizesTotalAmount = (sushiList) => {
+  let price = 0;
+  sushiList.forEach((sushi) => {
+    price = price + sushi.price * (sushi.size + 1);
+  });
+  return price;
 };
