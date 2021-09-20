81efb1a251bd046d54734f1cc48ad0e3eb1e7ca4
fedor <fedor993@gmail.com>
2021-09-20 15:56:23 +0300

    Implemented actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -5,7 +5,31 @@ export const SET_CATEGORY_TYPE = "SET_CATEGORY_TYPE";
 export const SET_SUSHI = "SET_SUSHI";
 export const SET_LOADING = "SET_LOADING";
 export const ADD_SUSHI = "ADD_SUSHI";
+export const CLEAR_CART = "CLEAR_CART";
+export const ADD_ONE_SUSHI = "ADD_ONE_SUSHI";
+export const REMOVE_ONE_SUSHI = "REMOVE_ONE_SUSHI";
+export const REMOVE_SUSHI_BUNDLE = "REMOVE_SUSHI_BUNDLE";
+
 export const setLoading = () => ({ type: SET_LOADING });
+
+export const addOneSushi = (property) => ({
+  type: ADD_ONE_SUSHI,
+  payload: property,
+});
+
+export const removeOneSushi = (property) => ({
+  type: REMOVE_ONE_SUSHI,
+  payload: property,
+});
+
+export const clearCart = () => ({
+  type: CLEAR_CART,
+});
+
+export const removeSushiBundle = (property) => ({
+  type: REMOVE_SUSHI_BUNDLE,
+  payload: property,
+});
 
 export const setCategoryType = (type) => ({
   type: SET_CATEGORY_TYPE,
@@ -20,7 +44,7 @@ export const setSortType = (type) => ({
 export const addSushi = (sushi) => ({
   type: ADD_SUSHI,
   payload: sushi,
-})
+});
 
 export const setSushi = (sushi) => ({
   type: SET_SUSHI,
