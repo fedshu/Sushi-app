c9a824ab7044d1de72c45c6ce18d0eedb7043f69
fedor <fedor993@gmail.com>
2021-09-20 15:55:15 +0300

    Implemented reducers

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,15 +1,21 @@
-import { ADD_SUSHI } from "../actions";
+import {
+  ADD_SUSHI,
+  CLEAR_CART,
+  ADD_ONE_SUSHI,
+  REMOVE_ONE_SUSHI,
+  REMOVE_SUSHI_BUNDLE,
+} from "../actions";
 
 const initialState = {
   items: {},
-  selectedSushi: [],
+  sushiDividedBySize: [],
   totalPrice: 0,
   totalCount: 0,
 };
 
 export const cart = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_SUSHI:
+    case ADD_SUSHI: {
       const stateItems = state.items[action.payload.id];
       const updatedSushi = {
         ...state.items,
@@ -19,18 +25,125 @@ export const cart = (state = initialState, action) => {
       };
 
       const updatedSushiAsArray = Object.values(updatedSushi);
-      const sushiPrice = state.totalPrice + action.payload.price * (action.payload.size + 1);
+      const sushiPrice =
+        state.totalPrice + action.payload.price * (action.payload.size + 1);
 
       return {
         ...state,
         items: updatedSushi,
-        selectedSushi: getSushiSizeAndPrice(updatedSushiAsArray),
+        sushiDividedBySize: getSushiSizeAndPrice(updatedSushiAsArray),
         totalPrice: sushiPrice,
-        totalCount: updatedSushiAsArray.flat().length,
+        totalCount: getSushiCount(updatedSushi),
       };
+    }
+    case CLEAR_CART: {
+      return initialState;
+    }
+    case ADD_ONE_SUSHI: {
+      const { id, size, type, price } = action.payload;
+      const items = {
+        ...state.items,
+        [id]: [
+          ...state.items[id],
+          {
+            ...state.items[id][0],
+            type,
+            size,
+          },
+        ],
+      };
+
+      return {
+        ...state,
+        items,
+        sushiDividedBySize: state.sushiDividedBySize.map((sushi) => {
+          if (sushi.type === type && sushi.id === id) {
+            sushi.totalPrice += sushi.price * (sushi.size + 1);
+            sushi.sizeCount += 1;
+          }
+          return sushi;
+        }),
+        totalPrice: state.totalPrice + price * (size + 1),
+        totalCount: state.totalCount + 1,
+      };
+    }
+    case REMOVE_ONE_SUSHI: {
+      const { id, size, type, price } = action.payload;
+      let items = {};
+      let sushiItemIndex;
+
+      if (state.items[id]) {
+        sushiItemIndex = state.items[id].findIndex(
+          (sushi) => sushi.type === type && sushi.id === id
+        );
+      }
+      if (state.items[id].length) {
+        state.items[id].splice(sushiItemIndex, 1);
+        items = {
+          ...state.items,
+          [id]: [...state.items[id]],
+        };
+      } else {
+      }
+
+      const sushiDividedBySize = [];
+      state.sushiDividedBySize.forEach((sushi) => {
+        if (sushi.type === type && sushi.id === id) {
+          if (sushi.sizeCount !== 1) {
+            sushi.totalPrice -= sushi.price * (sushi.size + 1);
+            sushi.sizeCount -= 1;
+            sushiDividedBySize.push(sushi);
+          }
+        } else {
+          sushiDividedBySize.push(sushi);
+        }
+      });
+
+      return {
+        ...state,
+        items,
+        sushiDividedBySize,
+        totalPrice: state.totalPrice - price * (size + 1),
+        totalCount: state.totalCount - 1,
+      };
+    }
+    case REMOVE_SUSHI_BUNDLE: {
+      const { id, type } = action.payload;
+      let totalPrice = 0;
+      const items = {
+        ...state.items,
+        [id]: state.items[id].filter((sushi) => {
+          if (sushi.type !== type) {
+            return sushi;
+          }
+        }),
+      };
+
+      Object.values(items)
+        .flat()
+        .forEach((sushi) => {
+          totalPrice += sushi.price * (sushi.size + 1);
+        });
+
+      return {
+        ...state,
+        items,
+        totalCount: getSushiCount(items),
+        totalPrice,
+        sushiDividedBySize: [
+          ...state.sushiDividedBySize.filter(
+            (sushi) => sushi.type !== type || sushi.id !== id
+          ),
+        ],
+      };
+    }
     default:
       return state;
   }
+};
+
+const getSushiCount = (sushi) => {
+  return Object.values(sushi).flat().length;
 };
 
 const getSushiSizeAndPrice = (sushi) => {
@@ -66,7 +179,7 @@ const getUniqueSushiSizes = (sushi) => {
 const getSushiSizesTotalAmount = (sushiList) => {
   let price = 0;
   sushiList.forEach((sushi) => {
-    price = price + sushi.price * (sushi.size + 1);
+    price += sushi.price * (sushi.size + 1);
   });
   return price;
 };
